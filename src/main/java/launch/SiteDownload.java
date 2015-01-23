c3b3e7d296ad8c7fe2980b8ba4a44e7830b74742
Partizanin <topiciv1@yandex.ua>
2015-01-23 20:01:09 +0200

    catch unknownHostException add readFrom file if exception add write to file if no exception

--- src/main/java/launch/SiteDownload.java
+++ src/main/java/launch/SiteDownload.java
@@ -1,11 +1,10 @@
 package launch;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
+import java.io.*;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  * Created with Intellij IDEA.
@@ -18,33 +17,42 @@ import java.net.URL;
 public class SiteDownload {
 
     public static void main(String[] args) {
+/*
+
+        String s = getSource();
+
+        System.out.println(s);
+*/
+
+
+        StringBuilder source = new StringBuilder();
         URL url;
         InputStream is = null;
         BufferedReader br = null;
         String line = "";
-        /*LocalTime time = LocalTime.now(ZoneId.systemDefault());*/
         try {
-            url = new URL("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22UAHEUR%22%2C%22UAHUSD%22%2C%22UAHRUB%22%2C%22USDRUB%22%2C%22USDEUR%22%2C%22USDUAH%22%2C%22EURRUB%22%2C%22EURUSD%22%2C%22EURUAH%22%2C%22RUBUSD%22%2C%22RUBEUR%22%2C%22RUBUAH%22)&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys");
-          /*  System.out.println("set url " + time);
-            System.out.println("start open stream " + time);
-          */  is = url.openStream();
-            /*System.out.println("stream is opened " + time);
-            System.out.println("start bufferReader " + time);
-            */br = new BufferedReader(new InputStreamReader(is));
-            /*System.out.println("bufferReader is started " + time);
-            System.out.println("start read br " + time);
-*/
+            url = new URL("https://query.yahooapi.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(\"UAHEUR\"%2C\"UAHUSD\"%2C\"UAHRUB\"%2C\"UAHPLN\"%2C\"USDEUR\"%2C\"USDUAH\"%2C\"USDRUB\"%2C\"USDPLN\"%2C\"EURUSD\"%2C\"EURUAH\"%2C\"EURRUB\"%2C\"EURPLN\"%2C\"RUBUAH\"%2C\"RUBEUR\"%2C\"RUBUSD\"%2C\"RUBPLN\"%2C\"PLNRUB\"%2C\"PLNEUR\"%2C\"PLNUSD\"%2C\"PLNUAH\")&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys");
+            is = url.openStream();
+            br = new BufferedReader(new InputStreamReader(is));
+
             while ((line = br.readLine()) != null){
-                System.out.println(line);
+                source.append(line);
             }
 
         } catch (IOException e) {
             e.printStackTrace();
+            try {
+                readFromFile();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+
         } finally {
             if (is != null) {
                 try {
                     is.close();
                     if (br != null) {
+                        writeToFile(source.toString());
                         br.close();
                     }
                 } catch (IOException e) {
@@ -54,28 +62,67 @@ public class SiteDownload {
 
         }
 
+        System.out.println(source);
     }
 
     public String getSource() {
-        HttpURLConnection con = null;
+        StringBuilder source = new StringBuilder();
+        URL url;
+        InputStream is = null;
+        BufferedReader br = null;
         String line = "";
         try {
-            URL url = new URL("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(\"UAHEUR\"%2C\"UAHUSD\"%2C\"UAHRUB\"%2C\"UAHPLN\"%2C\"USDEUR\"%2C\"USDUAH\"%2C\"USDRUB\"%2C\"USDPLN\"%2C\"EURUSD\"%2C\"EURUAH\"%2C\"EURRUB\"%2C\"EURPLN\"%2C\"RUBUAH\"%2C\"RUBEUR\"%2C\"RUBUSD\"%2C\"RUBPLN\"%2C\"PLNRUB\"%2C\"PLNEUR\"%2C\"PLNUSD\"%2C\"PLNUAH\")&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys");
-            con = (HttpURLConnection) url.openConnection();
-            con.setRequestProperty("User-Agent", "Mozilla/5.0");
-            BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"));//"Cp1251"
-            StringBuffer sb = new StringBuffer();
-            while ((line = br.readLine()) != null) {
-                sb.append(line).append("\n");
+            url = new URL("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(\"UAHEUR\"%2C\"UAHUSD\"%2C\"UAHRUB\"%2C\"UAHPLN\"%2C\"USDEUR\"%2C\"USDUAH\"%2C\"USDRUB\"%2C\"USDPLN\"%2C\"EURUSD\"%2C\"EURUAH\"%2C\"EURRUB\"%2C\"EURPLN\"%2C\"RUBUAH\"%2C\"RUBEUR\"%2C\"RUBUSD\"%2C\"RUBPLN\"%2C\"PLNRUB\"%2C\"PLNEUR\"%2C\"PLNUSD\"%2C\"PLNUAH\")&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys");
+            is = url.openStream();
+            br = new BufferedReader(new InputStreamReader(is));
 
+            while ((line = br.readLine()) != null){
+                source.append(line);
             }
-            line += sb;
+
         } catch (IOException e) {
-            e.printStackTrace();
+            /*e.printStackTrace();*/
+            System.err.println(e.getClass());
+            try {
+                source.append(readFromFile());
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+
         } finally {
-            assert con != null;
-            con.disconnect();
+            if (is != null) {
+                try {
+                    is.close();
+                    if (br != null) {
+                        writeToFile(source.toString());
+                        br.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
         }
-        return line;
+        return source.toString();
+    }
+
+    private static void writeToFile(String source){
+        PrintWriter pw = null;
+        try {
+             pw = new PrintWriter("source.txt");
+            pw.print(source);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }finally {
+            if (pw != null) {
+                pw.close();
+            }
+        }
+    }
+
+    private static String readFromFile() throws IOException {
+
+        byte[] encoded = Files.readAllBytes(Paths.get("source.txt"));
+        return new String(encoded, StandardCharsets.UTF_8);
     }
 }
